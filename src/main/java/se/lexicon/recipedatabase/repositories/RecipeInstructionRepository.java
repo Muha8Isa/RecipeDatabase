d43f8a08fe112ba178cdc22061c8be1ef9d50ffd
Mohammad Alissa <mohammad.alissa95@hotmail.com>
2023-02-01 13:33:26 +0100

    RecipeInstruction Repository added @Repository

--- src/main/java/se/lexicon/recipedatabase/repositories/RecipeInstructionRepository.java
+++ src/main/java/se/lexicon/recipedatabase/repositories/RecipeInstructionRepository.java
@@ -1,7 +1,9 @@
 package se.lexicon.recipedatabase.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import se.lexicon.recipedatabase.classes.RecipeInstruction;
 
+@Repository
 public interface RecipeInstructionRepository extends CrudRepository<RecipeInstruction, Integer> {
 }
