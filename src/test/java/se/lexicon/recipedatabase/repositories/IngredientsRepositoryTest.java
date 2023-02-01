dd03f75abb3f47b70124ccd2d2fa5cc7487dfb02
Mattias Bjoerk <mattias.bjork.id1@gmail.com>
2023-02-01 14:37:51 +0100

    did things 2

--- src/test/java/se/lexicon/recipedatabase/repositories/IngredientsRepositoryTest.java
+++ src/test/java/se/lexicon/recipedatabase/repositories/IngredientsRepositoryTest.java
@@ -2,30 +2,25 @@ package se.lexicon.recipedatabase.repositories;
 
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import se.lexicon.recipedatabase.classes.Ingredient;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
-@SpringBootTest
-@AutoConfigureTestDatabase
-@Transactional
-@AutoConfigureTestEntityManager
-@DirtiesContext
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
 public class IngredientsRepositoryTest {
 
     @Autowired
-    TestEntityManager em;
-
-    @Autowired
     IngredientsRepository testObject;
+
+    Ingredient createdIngredient1;
+    Ingredient createdIngredient2;
+    List<Ingredient> ingredientList;
 
     @BeforeEach
     public void setup(){
@@ -33,67 +28,34 @@ public class IngredientsRepositoryTest {
         Ingredient ingredient1 = new Ingredient("garlic");
         Ingredient ingredient2 = new Ingredient("onion");
 
-        Ingredient createdIngredient1 = em.persist(ingredient1);
-        Ingredient createdIngredient2 = em.persist(ingredient2);
+        createdIngredient1 = testObject.save(ingredient1);
+        createdIngredient2 = testObject.save(ingredient1);
+        assertNotNull(createdIngredient1);
+        assertNotNull(createdIngredient2);
 
-        int createdIngredient1Id = createdIngredient1.getId();
-        int createdIngredient2Id = createdIngredient2.getId();
+        ingredientList.add(createdIngredient1);
+        ingredientList.add(createdIngredient2);
+
 
     }
 
-    public Optional<Ingredient> findByIngredientIgnoreCase(String ingredient) {
-        return testObject.findByIngredientIgnoreCase(ingredient);
+    @Test
+    public void findByIngredientIgnoreCase() {
+
+        Optional<Ingredient> ingredientOptional = testObject.findByIngredientIgnoreCase(createdIngredient1.getIngredientName());
+        assertTrue(ingredientOptional.isPresent());
+        Ingredient actualData = ingredientOptional.get();
+        Ingredient expectedData = createdIngredient1;
+        assertEquals(expectedData, actualData);
+
     }
 
-    public List<Ingredient> findAllByIngredientContainsIgnoreCase(String ingredient) {
-        return testObject.findAllByIngredientContainsIgnoreCase(ingredient);
-    }
+    @Test
+    public void findAllByIngredientContainsIgnoreCase() {
 
-    public <S extends Ingredient> S save(S entity) {
-        return testObject.save(entity);
-    }
+        List<Ingredient> actualList = testObject.findAllByIngredientContainsIgnoreCase("I");
+        List<Ingredient> expectedList = ingredientList;
+        assertEquals(expectedList, actualList);
 
-    public <S extends Ingredient> Iterable<S> saveAll(Iterable<S> entities) {
-        return testObject.saveAll(entities);
-    }
-
-    public Optional<Ingredient> findById(Integer integer) {
-        return testObject.findById(integer);
-    }
-
-    public boolean existsById(Integer integer) {
-        return testObject.existsById(integer);
-    }
-
-    public Iterable<Ingredient> findAll() {
-        return testObject.findAll();
-    }
-
-    public Iterable<Ingredient> findAllById(Iterable<Integer> integers) {
-        return testObject.findAllById(integers);
-    }
-
-    public long count() {
-        return testObject.count();
-    }
-
-    public void deleteById(Integer integer) {
-        testObject.deleteById(integer);
-    }
-
-    public void delete(Ingredient entity) {
-        testObject.delete(entity);
-    }
-
-    public void deleteAllById(Iterable<? extends Integer> integers) {
-        testObject.deleteAllById(integers);
-    }
-
-    public void deleteAll(Iterable<? extends Ingredient> entities) {
-        testObject.deleteAll(entities);
-    }
-
-    public void deleteAll() {
-        testObject.deleteAll();
     }
 }
